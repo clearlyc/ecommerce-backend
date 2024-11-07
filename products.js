e67fc9120d41adf4022b6c96fede408db143612d
Ioel Katz <ioelkatz@gmail.com>
2024-11-07 10:14:31 -0300

    img carpets

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const products = [
     model: "Mustang GT Fastback",
     description:
       "A two-door, four-seat sports coupe with a 5.0-liter V8 engine that produces 460 horsepower and 420 pound-feet of torque",
-    image: "/img/Ford-Mustang.png",
+    image: "/img/Ford/Ford-Mustang.png",
     featured: true,
     price: 40000,
     stock: 5,
@@ -13,7 +13,7 @@ const products = [
     model: "MANSORY Stallone 812",
     description:
       "A stellar car with a strong name meet MANSORY Stallone, based on Ferrari 812 Superfast. The third generation of a fully refined Ferrari gran turismo is defined by lightweight bodywork components made of visible carbon fibre and several engine modifications, making this stallion an untamed beast.",
-    image: "/img/Ferrari-Stallone.png",
+    image: "/img/Ferrari/Ferrari-Stallone.png",
     featured: false,
     price: 899998,
     stock: 5,
@@ -23,7 +23,7 @@ const products = [
     model: "458 Spider",
     description:
       "The 458 Spider is the first car ever to combine a mid-rear engine with a retractable folding hard top that delivers both unprecedented in-cabin comfort when closed and unparalleled Spider performance.",
-    image: "/img/Ferrari-458Spider.png",
+    image: "/img/Ferrari/Ferrari-458Spider.png",
     featured: false,
     price: 640000,
     stock: 3,
@@ -33,7 +33,7 @@ const products = [
     model: "328 GTB",
     description:
       "The 308 series had won universal acclaim from both customers and critics, and its natural evolution continued with the 328 GTB, a berlinetta fitted with the new 3.2-litre version of the V8.",
-    image: "/img/Ferrari-328GTB.png",
+    image: "/img/Ferrari/Ferrari-328GTB.png",
     featured: false,
     price: 130000,
     stock: 4,
@@ -43,7 +43,7 @@ const products = [
     model: "SP48 Unica",
     description:
       "Designed by the Ferrari Styling Centre under the direction of Flavio Manzoni, Chief Design Officer, is a two-seater sports berlinetta developed on the F8 Tributo platform.",
-    image: "/img/Ferrari-SP48Unica.png",
+    image: "/img/Ferrari/Ferrari-SP48Unica.png",
     featured: true,
     price: 250000,
     stock: 3,
@@ -53,7 +53,7 @@ const products = [
     model: "288 GTO",
     description:
       "Twin-turbo engine, use of special materials, Pininfarina styling. The first production model with the features of a racing car. The Ferrari GTO",
-    image: "/img/Ferrari-GTO.png",
+    image: "/img/Ferrari/Ferrari-GTO.png",
     featured: false,
     price: 3250000,
     stock: 2,
@@ -63,7 +63,7 @@ const products = [
     model: "296 GT3",
     description:
       "The Ferrari 296 GT3 is a versatile car, developed with the needs of gentleman drivers in mind, both those who aim to win and those who relish racing on the track.",
-    image: "/img/Ferrari-296GT3.png",
+    image: "/img/Ferrari/Ferrari-296GT3.png",
     featured: true,
     price: 820000,
     stock: 3,
@@ -73,7 +73,7 @@ const products = [
     model: "904 Carrera GTS",
     description:
       "The Porsche 904 with its small frontal area of only 1.4 square metres, designed by F.A. Porsche, is the first Porsche with a fibre glass-reinforced plastic body.",
-    image: "/img/Porsche-1964-CarreraGTS.png",
+    image: "/img/Porsche/Porsche-1964-CarreraGTS.png",
     featured: false,
     price: 2535000,
     stock: 3,
@@ -83,7 +83,7 @@ const products = [
     model: "Mission X",
     description:
       "Flat body, sculptural styling, closed design — the exterior design of the Mission X is both innovative and distinctively Porsche. From the light structure and Le-Mans-style doors to the integrated, adaptive wings.",
-    image: "/img/Porsche-MissionX.png",
+    image: "/img/Porsche/Porsche-MissionX.png",
     featured: true,
     price: 1850000,
     stock: 3,
@@ -93,7 +93,7 @@ const products = [
     model: "Electric Phantom",
     description:
       "A luxury car known for its opulent interior, iconic design, and advanced technology.",
-    image: "/img/RollsRoyce-ModdedElectricPhantom.png",
+    image: "/img/RollsRoyce/RollsRoyce-ModdedElectricPhantom.png",
     featured: false,
     price: 495000,
     stock: 7,
@@ -103,7 +103,7 @@ const products = [
     model: "103 EX",
     description:
       " Rolls-Royce 103EX is our first ever vision vehicle, and embodies just one of the endless possibilities that awaits. A personalised icon for the future, providing a sanctuary of calm in an ever-faster world. ",
-    image: "/img/RollsRoyce-103Ex.png",
+    image: "/img/RollsRoyce/RollsRoyce-103Ex.png",
     featured: false,
     price: 1800000,
     stock: 7,
@@ -113,18 +113,8 @@ const products = [
     model: "911 GT3",
     description:
       "For almost 25 years the 911 GT3 has accepted only the limits of physics — and left the ordinary far behind. Motorsport-tested design and maximum performance ensure it unwaveringly bridges the boundary between the racetrack and the road.",
-    image: "/img/Porsche-911GT3.png",
+    image: "/img/Porsche/Porsche-911GT3.png",
     featured: false,
-    price: 222500,
-    stock: 9,
-    year: 2024,
-  },
-  {
-    model: "PRUEBA",
-    description:
-      "For almost 25 years the 911 GT3 has accepted only the limits of physics — and left the ordinary far behind. Motorsport-tested design and maximum performance ensure it unwaveringly bridges the boundary between the racetrack and the road.",
-    image: "/img/image.png",
-    featured: true,
     price: 222500,
     stock: 9,
     year: 2024,
