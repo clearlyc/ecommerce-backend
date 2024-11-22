d41848986ceaa1376225ee02cc3422295b5ed9e5
fortizsenattore <fortizsenattore@gmail.com>
2024-11-22 10:37:55 -0300

    v 20/11

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const emailCheck = require("../middlewares/emailCheck");
 
-router.get("/", userController.index);
+router.get("/", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] },userController.index);
 router.post("/", userController.store);
 router.post("/email", userController.show);
 router.patch("/", userController.update);
-router.patch("/:id", userController.profileUpdate);
+router.patch("/:id", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }, userController.profileUpdate));
 router.delete("/:id", userController.destroy);
 
 module.exports = router;
